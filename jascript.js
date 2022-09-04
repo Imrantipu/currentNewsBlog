94f141cb7621c5bd82a34c01cdecab1cb282d493
Imran <imrantipu90@yahoo.com>
2022-09-04 08:35:29 +0200

    error handeling tried display catagory rechecked

--- jascript.js
+++ jascript.js
@@ -1,21 +1,35 @@
-const loadNews = async() =>
-{
+const loader = document.querySelector("#loader");
+loader.innerHTML = `
+<div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>
+`;
+
+loader.style.display = "none";
+try {
+  const loadNewsCategories = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     const res = await fetch(url);
     const data = await res.json();
     displayCategories(data.data.news_category);
+  };
+  const displayNewsCategories = loadNewsCategories();
+} catch (error) {
+  console.error(error);
 }
 
 //-----display-category--------
-const displayCategories = categories => {
-    const categoriesContainer = document.getElementById('categories-container');
-    categoriesContainer.innerHTML = ``;
-    categories.forEach(category =>{
-        const categoryDiv = document.createElement('div');
-        categoryDiv.innerHTML = `
-        <p>${category.category_name}</p>
+const displayCategories = (categories) => {
+  const categoriesContainer = document.getElementById("categories-container");
+  categoriesContainer.innerHTML = ``;
+  categories.forEach((category) => {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML = `
+        <a href="#" class='show-news' onclick="getNews('${category.category_id}')" class='text-decoration-none'>${category.category_name}</a>
         `;
-        categoriesContainer.appendChild(categoryDiv);
-    })
-}
-loadNews();
+    categoriesContainer.appendChild(categoryDiv);
+    categoriesContainer.classList.add("text-decoration-none");
+  });
+};
